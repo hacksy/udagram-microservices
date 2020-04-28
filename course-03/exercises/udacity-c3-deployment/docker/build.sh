3be278cf4a39938389690a186233297bdb1042be
Hansy Schmitt <github@hansyschmitt.com>
2020-04-28 18:57:49 -0500

    Fix typo on DOCKER_USER

--- course-03/exercises/udacity-c3-deployment/docker/build.sh
+++ course-03/exercises/udacity-c3-deployment/docker/build.sh
@@ -4,7 +4,7 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 echo "master build"
 docker-compose -f udacity-c3-deployment/docker/docker-compose-build.yaml build --parallel
 docker images
-docker push $DOCKER_USERNAME/udacity-restapi-user
-docker push $DOCKER_USERNAME/udacity-restapi-feed
-docker push $DOCKER_USERNAME/udacity-frontend
+docker push $DOCKER_USER/udacity-restapi-user
+docker push $DOCKER_USER/udacity-restapi-feed
+docker push $DOCKER_USER/udacity-frontend
 docker logout
