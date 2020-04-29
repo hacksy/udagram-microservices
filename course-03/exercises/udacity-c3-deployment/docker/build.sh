f0e3c7573e50913e0fdbd9db6375bc269446694f
Hansy Schmitt <github@hansyschmitt.com>
2020-04-28 20:29:52 -0500

    Add Screenshots of dockerhub

--- course-03/exercises/udacity-c3-deployment/docker/build.sh
+++ course-03/exercises/udacity-c3-deployment/docker/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo $TRAVIS_BRANCH
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-echo "master build"
 docker-compose -f course-03/exercises/udacity-c3-deployment/docker/docker-compose-build.yaml build --parallel
 docker images
 docker push $DOCKER_USER/udacity-restapi-user
